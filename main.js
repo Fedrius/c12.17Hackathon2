5fcc2fc50b874aa8c3ce7c0507540c37d17c1cca
wrongeric <33172384+wrongeric@users.noreply.github.com>
2018-01-17 10:13:40 -0800

    change to only add 4 photo divs for styling purposes

--- main.js
+++ main.js
@@ -228,7 +228,7 @@ var flickrClickHandler = function(beachName) {
         url: `https://api.flickr.com/services/rest?method=flickr.photos.search&api_key=629e34714d717373e24940da3b0ad6cb&format=json&nojsoncallback=1&text=${flickrSearch} sunset&per_page=10`,
         success: function(data) {
             dataFromServer = data;
-            for(let dataIndex = 0; dataIndex < 10; dataIndex++) {
+            for(let dataIndex = 0; dataIndex < 4; dataIndex++) {
                 var dataObj = {
                     id: dataFromServer.photos.photo[dataIndex].id,
                     server: dataFromServer.photos.photo[dataIndex].server,
@@ -245,92 +245,6 @@ var flickrClickHandler = function(beachName) {
     }
     $.ajax(ajaxConfig);
 }
-// // Newport Beach Click Handler
-// var nbClickHandler = function() {
-//     beachPhotoArrayData = [];
-//     beachPhotoArray = [];
-//     var dataFromServer;
-//     var ajaxConfig = {
-//         method: "GET",
-//         text: 'newport beach', // input field needs to change text using jQuery
-//         url: 'https://api.flickr.com/services/rest?method=flickr.photos.search&api_key=629e34714d717373e24940da3b0ad6cb&format=json&nojsoncallback=1&text=newport beach&per_page=10',
-//         success: function (data) {
-//             dataFromServer = data;
-//             for (let dataIndex = 0; dataIndex < 5; dataIndex++) {
-//                 var dataObj = {
-//                     id: dataFromServer.photos.photo[dataIndex].id,
-//                     server: dataFromServer.photos.photo[dataIndex].server,
-//                     farm: dataFromServer.photos.photo[dataIndex].farm,
-//                     secret: dataFromServer.photos.photo[dataIndex].secret,
-//                 };
-//                 beachPhotoArrayData.push(dataObj);
-//             }
-//             makePhotoURL(beachPhotoArrayData);
-//         },
-//             error: function(error){
-//                 console.log(error);
-//         }
-//     }
-//     $.ajax(ajaxConfig);
-// }
-// // Sunset Beach Click Handler
-// var sunsetBeachClickHandler = function() {
-//     beachPhotoArrayData = [];
-//     beachPhotoArray = [];
-//     var dataFromServer;
-//     var ajaxConfig = {
-//         method: "GET",
-//         dataType: 'json',
-//         text: 'sunset beach', // input field needs to change text using jQuery later on will make make a dynamic variable
-//         url: 'https://api.flickr.com/services/rest?method=flickr.photos.search&api_key=629e34714d717373e24940da3b0ad6cb&format=json&nojsoncallback=1&text=sunset beach surf&per_page=10',
-//         success: function (data) {
-//             dataFromServer = data;
-//             for (let dataIndex = 0; dataIndex < 5; dataIndex++) {
-//                 var dataObj = {
-//                     id: dataFromServer.photos.photo[dataIndex].id,
-//                     server: dataFromServer.photos.photo[dataIndex].server,
-//                     farm: dataFromServer.photos.photo[dataIndex].farm,
-//                     secret: dataFromServer.photos.photo[dataIndex].secret,
-//                 };
-//                 beachPhotoArrayData.push(dataObj);
-//             }
-//             makePhotoURL(beachPhotoArrayData);
-//         },
-//             error: function(error){
-//                 console.log(error);
-//             }
-//     }
-//     $.ajax(ajaxConfig);
-// }
-//
-// //Seal Beach Click Handler for Ajax Call
-// var sealBeachClickHandler = function() {
-//     beachPhotoArrayData = [];
-//     beachPhotoArray = [];
-//     var dataFromServer;
-//     var ajaxConfig = {
-//         method: "GET",
-//         text: 'seal beach', // input field needs to change text using jQuery
-//         url: 'https://api.flickr.com/services/rest?method=flickr.photos.search&api_key=629e34714d717373e24940da3b0ad6cb&format=json&nojsoncallback=1&text=seal beach surf&per_page=10',
-//         success: function (data) {
-//             dataFromServer = data;
-//             for (let dataIndex = 0; dataIndex < 5; dataIndex++) {
-//                 var dataObj = {
-//                     id: dataFromServer.photos.photo[dataIndex].id,
-//                     server: dataFromServer.photos.photo[dataIndex].server,
-//                     farm: dataFromServer.photos.photo[dataIndex].farm,
-//                     secret: dataFromServer.photos.photo[dataIndex].secret,
-//                 };
-//                 beachPhotoArrayData.push(dataObj);
-//             }
-//             makePhotoURL(beachPhotoArrayData);
-//         },
-//         error: function (error) {
-//             console.log(error);
-//         }
-//     }
-//     $.ajax(ajaxConfig);
-// }
 
 var showModal = function(){
     var backgroundImage = $(event.currentTarget).css('background-image');
